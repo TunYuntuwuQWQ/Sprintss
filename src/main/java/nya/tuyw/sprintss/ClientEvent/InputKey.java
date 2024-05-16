d5065179c6e11db04b2c711db555429774a06451
TunYuntuwuQWQ <tunyuntuwu__0@163.com>
2024-05-16 14:09:45 +0800

    ??

--- src/main/java/nya/tuyw/sprintss/ClientEvent/InputKey.java
+++ src/main/java/nya/tuyw/sprintss/ClientEvent/InputKey.java
@@ -4,6 +4,7 @@ package nya.tuyw.sprintss.ClientEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.phys.Vec3;
@@ -32,12 +33,13 @@ public class InputKey {
                 f1 *= f5 / f4;
                 f2 *= f5 / f4;
                 f3 *= f5 / f4;
-                player.push((double)f1, (double)f2, (double)f3);
+                player.push(f1, f2, f3);
                 if (player.onGround()) {
                     player.move(MoverType.SELF, new Vec3(0.0D, 1.4F, 0.0D));
                 }
-
+                Minecraft.getInstance().level.playSound(null,player,Sounds.SPRINT.get(), SoundSource.PLAYERS,1.0F,1.0F);
                 player.playSound(Sounds.SPRINT.get());
+                player.playSound(Sounds.WAAH.get());
                 player.sendSystemMessage(Component.translatable("message.key.sprint"));
             }else {
                 player.sendSystemMessage(Component.translatable("message.key.cooldown"));
